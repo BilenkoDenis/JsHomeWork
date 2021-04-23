ac1ac05f58a5d0c72db90fc6febd11c8f70dee90
Denis Bilenko <bilenko.deniss@gmail.com>
2021-04-23 22:28:10 +0300

    one more fix

--- HomeWork_1/script.js
+++ HomeWork_1/script.js
@@ -1,4 +1,4 @@
 const askName = 'What is your name?';
 let name = prompt(askName);
-let greeting = 'Hello,' + ' ' + name + '! ' + 'How are you ?';
+let greeting = 'Hello, ' + name + '! How are you ?';
 alert(greeting);
