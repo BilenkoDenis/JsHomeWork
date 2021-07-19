904576770cf59ca4f30597f2e73b82f0e83cdc65
Denis Bilenko <bilenko.deniss@gmail.com>
2021-07-19 14:57:28 +0300

    add hw_18

--- hw_18/script.js
+++ hw_18/script.js
@@ -1,88 +1,157 @@
+const CONTACTS_URL = `https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/users/`;
+
 const REGEX = /^[+]*[(]?[0-9]{1,3}[)]?[-\s/0-9]*$/g;
-const firstNameInput = document.getElementById("firstName");
-const secondNameInput = document.getElementById("secondName");
-const phoneInput = document.getElementById("phoneNumber");
-const tbodyEl = document.querySelector("#tbody");
+const REMOVE_CLASS = `remove-item`;
+const TABLE_ROW_CLASS = `person-info`;
+const BTN_CLASS = `d-flex justify-content-evenly`;
+const TABLE_DATA_CLASS = `text-center`;
 
-document.querySelector(".btn").addEventListener("click", applyEl);
-tbodyEl.addEventListener("click", removeTableRow);
+const nameInput = document.getElementById(`name`);
+const phoneInput = document.getElementById(`phoneNumber`);
+const emailInput = document.getElementById(`email`);
+const tbodyEl = document.getElementById(`tbody`);
+const btn = document.getElementById(`addContactForm`);
 
-function applyEl() {
-    const tableEl = createEl();
+let contactList = [];
+
+init();
+
+btn.addEventListener(`submit`, onAddContactFormSubmit);
+tbodyEl.addEventListener(`click`, editTableRow);
+
+function onAddContactFormSubmit(event) {
+    event.preventDefault();
+    submitForm();
     resetInputs();
-    tbodyEl.appendChild(tableEl);
+}
+function init() {
+    fetchContacts();
 }
 
-function isTextInputInvalid(str) {
-    return str.trim() !== "" && isNaN(str);
-}
-
-function isPhoneInputInvalid(str) {
-    return REGEX.test(str);
-}
-
-function isInputsInvalid() {
-    return isTextInputInvalid(firstNameInput.value) &&
-           isTextInputInvalid(secondNameInput.value) &&
-           isPhoneInputInvalid(phoneInput.value)
-}
-
-function createEl() {
-    const tableRow = createTableRowEl();
-
-    if (isInputsInvalid()) {
-        tableRow.appendChild(createTableDataEl(firstNameInput.value));
-        tableRow.appendChild(createTableDataEl(secondNameInput.value));
-        tableRow.appendChild(createTableDataEl(phoneInput.value));
-        tableRow.appendChild(createRemoveButtonEl());
+function submitForm() {
+    if (!inputValidation()) {
+        return;
     }
-
-    return tableRow;
+    const contact = getFormData();
+    createContact(contact);
 }
 
-function createTableRowEl() {
-    const tr = document.createElement("tr");
-    tr.className = "person-info";
+function createContact(contact) {
+    fetch(CONTACTS_URL, {
+        method: `POST`,
+        body: JSON.stringify(contact),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((resp) => resp.json())
+        .then(addContact);
+}
+function addContact(contact) {
+    contactList.push(contact);
+    renderContacts(contactList);
+}
+function getFormData() {
+    return {
+        name: nameInput.value,
+        phone: phoneInput.value,
+        email: emailInput.value,
+    };
+}
 
+function inputValidation() {
+    return (
+        isInputValid(nameInput.value) &&
+        isPhoneInputValid(phoneInput.value) &&
+        isInputValid(emailInput.value)
+    );
+}
+
+function createTableRow(id) {
+    const tr = document.createElement(`tr`);
+    tr.className = TABLE_ROW_CLASS;
+    tr.id = id;
     return tr;
 }
 
-function createTableDataEl(input) {
-    const td = createTestEl();
-    td.innerText = input;
-
-    return td;
-}
-
-function createRemoveButtonEl() {
-    const el = createTestEl();
-
-    const link = document.createElement("a");
-    link.className = "remove-item";
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-
-    el.appendChild(link);
-
+function createButtons() {
+    const el = createTableData();
+    el.className = BTN_CLASS;
+    el.appendChild(createDeleteButton());
     return el;
 }
 
-function createTestEl() {
-    const el = document.createElement("td");
-    el.className = "text-center";
-
+function createDeleteButton() {
+    const link = document.createElement("a");
+    link.className = REMOVE_CLASS;
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    return link;
+}
+function createTableData() {
+    const el = document.createElement(`td`);
+    el.className = TABLE_DATA_CLASS;
     return el;
 }
 function resetInputs() {
-    firstNameInput.value = "";
-    secondNameInput.value = "";
+    nameInput.value = "";
     phoneInput.value = "";
+    emailInput.value = "";
 }
-function removeTableRow(e) {
-    if (e.target.parentElement.classList.contains("remove-item")) {
-        removeListElement(e.target.closest(".person-info"));
+function editTableRow(e) {
+    switch (true) {
+        case e.target.parentElement.classList.contains(REMOVE_CLASS):
+            deleteContact(e.target.closest(`.` + TABLE_ROW_CLASS).id);
+            break;
     }
 }
 
-function removeListElement(el) {
-    el.remove();
+function isInputValid(str) {
+    return str.trim() !== "" && isNaN(str);
 }
+
+function isPhoneInputValid(str) {
+    return REGEX.test(str);
+}
+
+function fetchContacts() {
+    fetch(CONTACTS_URL)
+        .then((resp) => resp.json())
+        .then(setContacts)
+        .then(renderContacts);
+}
+
+function setContacts(data) {
+    return (contactList = data);
+}
+
+function renderContacts(data) {
+    tbodyEl.innerHTML = ``;
+    data.map(renderContact);
+}
+
+function renderContact({ id, name, phone, email }) {
+    const tableRow = createTableRow(id);
+    tableRow.appendChild(addInputToTd(name));
+    tableRow.appendChild(addInputToTd(phone));
+    tableRow.appendChild(addInputToTd(email));
+    tableRow.appendChild(createButtons());
+    tbodyEl.appendChild(tableRow);
+}
+
+function addInputToTd(value) {
+    const td = createTableData();
+    td.innerText = value;
+    return td;
+}
+
+function deleteContact(id) {
+    fetch(CONTACTS_URL + id, {
+        method: `DELETE`,
+    }).then(() => {
+        contactList = contactList.filter((item) => item.id !== id);
+        renderContacts(contactList);
+    });
+}
+
+
+
