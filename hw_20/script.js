6d585cf2eac2e99aa4cb0dc680489df7be6a505d
Denis Bilenko <bilenko.deniss@gmail.com>
2021-07-26 13:16:58 +0300

    add hw_20

---
+++ hw_20/script.js
@@ -0,0 +1,46 @@
+const container = $(`.fotorama`);
+const MAIN_URL = `https://jsonplaceholder.typicode.com/photos?_limit=10`;
+
+let photosList = [];
+
+init();
+
+function init() {
+    fetchPhotos();
+}
+
+function fetchPhotos() {
+    fetch(MAIN_URL)
+        .then((resp) => resp.json())
+        .then(setPhotos)
+        .then(renderPhotos);
+}
+
+function setPhotos(list) {
+    return (photosList = list);
+}
+function renderPhotos(list) {
+    container.empty();
+    list.map(renderPhoto);
+}
+function renderPhoto(list) {
+    let img = createImg(list);
+    img.appendTo(container);
+}
+
+function createImg(photo) {
+    //let link = createLink(photo);
+    let img = $(`<img/>`, {
+        src: photo.thumbnailUrl,
+        alt: photo.title,
+        id: photo.id,
+    });
+    img.appendTo(container);
+    return img;
+}
+
+function createLink(photo) {
+    let link = $(`<a/>`);
+    link.attr(`href`, photo.url);
+    return link;
+}
